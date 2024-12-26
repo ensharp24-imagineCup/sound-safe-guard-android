73eabf021b8e4d9c76d290ea2b4b7b806e3ee78a
SangHyeok0908 <tkdgur9799@gmail.com>
2024-12-26 20:58:57 +0900

    Feat: Navigation Compose 추가
    MainActivity 파일에 mainNavHost function을 추가하여 mainNavHost에서 NavHost를 정의한다.

--- app/src/main/java/net/azurewebsites/soundsafeguard/ui/MainActivity.kt
+++ app/src/main/java/net/azurewebsites/soundsafeguard/ui/MainActivity.kt
@@ -3,6 +3,11 @@ package net.azurewebsites.soundsafeguard.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import net.azurewebsites.soundsafeguard.ui.theme.SoundSafeGuardTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,8 +15,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SoundSafeGuardTheme {
-
+                mainNavHost()
             }
+        }
+    }
+}
+
+@Composable
+fun mainNavHost(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = rememberNavController(),
+        startDestination = "home"
+    ) {
+        composable("home") {
+            // Home 화면
         }
     }
 }
