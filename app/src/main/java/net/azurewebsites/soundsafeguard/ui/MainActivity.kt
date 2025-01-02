64fcfad75711fedf758e4ca4b91058b2682f4c5c
bellkite <aalicen44@gmail.com>
2025-01-02 20:18:20 +0900

    [add] StartScreen추가

--- app/src/main/java/net/azurewebsites/soundsafeguard/ui/MainActivity.kt
+++ app/src/main/java/net/azurewebsites/soundsafeguard/ui/MainActivity.kt
@@ -4,20 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import net.azurewebsites.soundsafeguard.ui.components.AppBar
-import net.azurewebsites.soundsafeguard.ui.theme.SoundSafeGuardTheme
+import net.azurewebsites.soundsafeguard.ui.screens.StartScreen
 import net.azurewebsites.soundsafeguard.ui.screens.MainScreen
-import net.azurewebsites.soundsafeguard.ui.screens.HomeScreen
 import net.azurewebsites.soundsafeguard.ui.screens.SoundSettingScreen
+import net.azurewebsites.soundsafeguard.ui.theme.SoundSafeGuardTheme
 import net.azurewebsites.soundsafeguard.viewmodel.SoundViewModel
-import androidx.compose.material3.Scaffold
 
-class MainActivity : ComponentActivity() {
+    class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -29,8 +29,8 @@ class MainActivity : ComponentActivity() {
                     }
                 ) { innerPadding ->
                     NavHost(navController, startDestination = "main", Modifier.padding(innerPadding)) {
-                        composable("home") { 
-                            HomeScreen(navController) 
+                        composable("start") {
+                            StartScreen()
                         }
                         composable("main") { 
                             MainScreen() 
