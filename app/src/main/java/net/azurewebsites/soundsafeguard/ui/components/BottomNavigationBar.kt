096dab9d16847ed06f97ea1d97d50ae56c2b46e0
huro0906 <huro0906@gmail.com>
2025-01-02 21:40:26 +0900

    Add: BottomNavigationBar 추가

---
+++ app/src/main/java/net/azurewebsites/soundsafeguard/ui/components/BottomNavigationBar.kt
@@ -0,0 +1,102 @@
+package net.azurewebsites.soundsafeguard.ui.components
+
+import android.content.Context
+import android.graphics.Color
+import androidx.annotation.StringRes
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import net.azurewebsites.soundsafeguard.R
+
+@Composable
+fun BottomNavigationBar(
+    navController: NavController,
+    containerColor: androidx.compose.ui.graphics.Color,
+    contentColor: androidx.compose.ui.graphics.Color,
+    indicatorColor: androidx.compose.ui.graphics.Color,
+    modifier: Modifier
+    ){
+
+    //navBar item
+    val items = listOf<BottomNavigationItem>(
+        BottomNavigationItem.Home,
+        BottomNavigationItem.Setting
+    )
+
+    //navController의 현 루트
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    AnimatedVisibility(
+        visible = items.map { it.route }.contains(currentRoute)
+    ) {
+        NavigationBar(
+            modifier = modifier,
+            containerColor = containerColor,
+            contentColor = contentColor
+        ) {
+            items.forEach { item ->
+                NavigationBarItem(
+                    selected = currentRoute == item.route,
+
+                    label = {
+                        Text(
+                            text = stringResource(id = item.title),
+                            style = TextStyle(
+                                fontSize = 12.sp
+                            )
+                        )
+                    },
+                    icon = {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = stringResource(id = item.route)
+                        )
+                    },
+                    onClick = {
+                        navController.navigate(item.route) {
+                            navController.graph.startDestinationRoute?.let {
+                                popUpTo(it){ saveState = true}
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+
+                        }
+                    }
+                )
+            }
+        }
+    }
+
+
+
+}
+
+
+sealed class BottomNavigationItem(
+    @StringRes val route: Int, val icon: ImageVector, val title: String){
+    object Home : BottomNavigationItem(R.string.,Icons.Default.Home , "Home")
+    object Setting : BottomNavigationItem("setting", Icons.Default.Settings, "Settings")
+}
