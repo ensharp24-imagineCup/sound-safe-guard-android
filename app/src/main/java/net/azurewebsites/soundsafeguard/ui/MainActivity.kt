a0c59050d27aa503e20410fd180ac6bf5ab7f676
bellkite <aalicen44@gmail.com>
2025-01-04 01:05:57 +0900

    [add] topbar

--- app/src/main/java/net/azurewebsites/soundsafeguard/ui/MainActivity.kt
+++ app/src/main/java/net/azurewebsites/soundsafeguard/ui/MainActivity.kt
@@ -42,6 +42,7 @@ class MainActivity : ComponentActivity() {
                         startDestination = "main",
                         Modifier.padding(innerPadding)
                     ) {
+
                         composable("start") {
                             StartScreen()
                         }
