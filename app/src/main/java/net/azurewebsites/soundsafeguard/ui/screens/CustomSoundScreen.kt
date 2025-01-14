d6e8263283a26f67b4695aea6baf8ba9a3b73fc8
bellkite <aalicen44@gmail.com>
2025-01-14 23:33:13 +0900

    Feat: add Dialog

--- app/src/main/java/net/azurewebsites/soundsafeguard/ui/screens/CustomSoundScreen.kt
+++ app/src/main/java/net/azurewebsites/soundsafeguard/ui/screens/CustomSoundScreen.kt
@@ -13,19 +13,27 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Icon
+import androidx.compose.material3.Icon
+import androidx.compose.material3.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Mic
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -41,6 +49,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
 import net.azurewebsites.soundsafeguard.R
 import net.azurewebsites.soundsafeguard.viewmodel.MainViewModel
@@ -55,7 +64,7 @@ fun CustomSoundScreen(
     val isActivated by mainViewModel.isActivated
     var isRecording by remember { mutableStateOf(false) }
     var showDialog by remember { mutableStateOf(false) }
-
+    var showConfirmationDialog by remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier.fillMaxSize().then(
@@ -78,7 +87,7 @@ fun CustomSoundScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
-                .padding(horizontal = 20.dp)
+                .padding(horizontal = 20.dp),
         ){
             CustomSoundSettingText(
                 modifier = Modifier.padding(top = 43.4.dp, bottom = 10.dp),
@@ -119,7 +128,7 @@ fun CustomSoundScreen(
                             .background(Color.White, CircleShape)
                     )
                 }
-                Icon( 
+                Icon(
                     imageVector = Icons.Default.Mic,
                     contentDescription = "Mic Icon",
                     modifier = Modifier.size(80.dp),
@@ -135,29 +144,99 @@ fun CustomSoundScreen(
                 fontSize = 24.sp,
                 fontFamily = FontFamily(Font(R.font.inter_regular)),
                 lineHeight = 18.sp, // 16포인트의 110%에 해당하는 줄 간격
-                //letterSpacing = (-4).sp, // -4%의 글자 간격
                 color = Color(0xFF888888)
             )
         }
 
         if (showDialog) {
-            AlertDialog(
-                onDismissRequest = { showDialog = false },
-                title = { Text("Custom Sound") },
-                text = { Text("Please input the name of the sound") },
-                confirmButton = {
-                    Button(onClick = { 
-                        showDialog = false 
-                    }) {
-                        Text("save")
-                    }
-                },
-                dismissButton = {
-                    Button(onClick = { showDialog = false }) {
-                        Text("cancel")
+            var soundName by remember { mutableStateOf("") }
+            Dialog(onDismissRequest = { showDialog = false }) {
+                Box(
+                    modifier = Modifier
+                        .size(355.dp, 235.dp)
+                        .background(Color.White, shape = RoundedCornerShape(15.dp))
+                        .padding(16.dp)
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        TextField(
+                            value = soundName,
+                            onValueChange = { soundName = it },
+                            placeholder = { Text("Sound name") },
+                            modifier = Modifier.size(252.dp, 51.dp)
+                        )
+
+                        Spacer(modifier = Modifier.height(19.dp))
+
+                        Text(
+                            "Please input the name of the sound",
+                            fontSize = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.inter_regular)),
+                            lineHeight = 18.sp,
+                            color = Color.Black
+                        )
+
+                        Spacer(modifier = Modifier.height(17.dp))
+
+                        Text(
+                            "It will be saved as the name you entered.",
+                            fontSize = 13.sp,
+                            fontFamily = FontFamily(Font(R.font.inter_regular)),
+                            lineHeight = 15.sp,
+                            color = Color(0xFF888888)
+                        )
+
+                        Spacer(modifier = Modifier.height(19.dp))
+
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceEvenly,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Button(
+                                onClick = { showDialog = false },
+                                colors = ButtonDefaults.buttonColors(Color(0xFFE0E3EB)),
+                                modifier = Modifier
+                                    .size(115.dp, 35.dp)
+                                    .background(Color(0xFFE0E3EB), shape = RoundedCornerShape(5.dp))
+                            ) {
+                                Text(
+                                    "cancel",
+                                    fontSize = 16.sp,
+                                    fontFamily = FontFamily(Font(R.font.inter_regular)),
+                                    lineHeight = 18.sp,
+                                    color = Color(0xFF888888)
+                                )
+                            }
+
+                            Button(
+                                onClick = {
+                                    showDialog = false
+                                    showConfirmationDialog = true
+                                },
+                                colors = ButtonDefaults.buttonColors(Color(0xFF5664E4)),
+                                modifier = Modifier
+                                    .size(115.dp, 35.dp)
+                                    .background(Color(0xFF5664E4), shape = RoundedCornerShape(5.dp))
+                            ) {
+                                Text(
+                                    "next",
+                                    fontSize = 16.sp,
+                                    fontFamily = FontFamily(Font(R.font.inter_regular)),
+                                    lineHeight = 18.sp,
+                                    color = Color.White
+                                )
+                            }
+                        }
                     }
                 }
-            )
+            }
+        }
+
+        if (showConfirmationDialog) {
+            ConfirmationDialog(onDismiss = { showConfirmationDialog = false })
         }
     }
 }
@@ -202,5 +281,86 @@ fun WaveEffect() {
             radius = 90.dp.toPx() * waveAnimation,
             center = center
         )
+    }
+}
+
+
+
+
+
+@Composable
+fun ConfirmationDialog(onDismiss: () -> Unit) {
+    Dialog(onDismissRequest = onDismiss) {
+        Box(
+            modifier = Modifier
+                .size(355.dp, 235.dp)
+                .background(Color.White, shape = RoundedCornerShape(15.dp))
+                .padding(16.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    "Do you want to save the recording?",
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily(Font(R.font.inter_regular)),
+                    lineHeight = 18.sp,
+                    color = Color.Black
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    "It will be saved as the name you entered.",
+                    fontSize = 13.sp,
+                    fontFamily = FontFamily(Font(R.font.inter_regular)),
+                    lineHeight = 15.sp,
+                    color = Color(0xFF888888)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Button(
+                        onClick = onDismiss,
+                        colors = ButtonDefaults.buttonColors(Color(0xFFE0E3EB)),
+                        modifier = Modifier
+                            .size(115.dp, 35.dp)
+                            .background(Color(0xFFE0E3EB), shape = RoundedCornerShape(5.dp))
+                    ) {
+                        Text(
+                            "cancel",
+                            fontSize = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.inter_regular)),
+                            lineHeight = 18.sp,
+                            color = Color(0xFF888888)
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(20.dp))
+
+                    Button(
+                        onClick = {
+                            onDismiss()
+                        },
+                        colors = ButtonDefaults.buttonColors(Color(0xFF5664E4)),
+                        modifier = Modifier
+                            .size(115.dp, 35.dp)
+                            .background(Color(0xFF5664E4), shape = RoundedCornerShape(5.dp))
+                    ) {
+                        Text(
+                            "save",
+                            fontSize = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.inter_regular)),
+                            lineHeight = 18.sp,
+                            color = Color.White
+                        )
+                    }
+                }
+            }
+        }
     }
 }
