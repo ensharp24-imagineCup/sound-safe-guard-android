89245d3609e00c25a78062334192ee66bfaeec89
서상혁 <tkdgur9799@gmail.com>
2025-01-15 12:25:30 +0900

    Feat: wear os 에서 보내는 audio byte array와 timestamp를 모바일에서 받아 useAudio 함수에서 사용한다.

--- app/src/main/java/net/azurewebsites/soundsafeguard/service/DataClientService.kt
+++ app/src/main/java/net/azurewebsites/soundsafeguard/service/DataClientService.kt
@@ -2,7 +2,6 @@ package net.azurewebsites.soundsafeguard.service
 
 import android.content.Context
 import android.util.Log
-import androidx.compose.runtime.mutableStateOf
 import com.google.android.gms.wearable.DataClient
 import com.google.android.gms.wearable.DataEvent
 import com.google.android.gms.wearable.DataEventBuffer
@@ -11,32 +10,30 @@ import com.google.android.gms.wearable.Wearable
 
 class DataClientService : DataClient.OnDataChangedListener {
 
-    private var receivedMessage = mutableStateOf("")
-    fun getReceivedMessage() = receivedMessage.value
-
     fun registerDataClient(context: Context) {
-        Log.d("DataClientService", "Data client registered")
         Wearable.getDataClient(context).addListener(this)
     }
 
     fun unregisterDataClient(context: Context) {
-        Log.d("DataClientService", "Data client unregistered")
         Wearable.getDataClient(context).removeListener(this)
     }
 
     override fun onDataChanged(dataEventBuffer: DataEventBuffer) {
-        Log.d("DataClientService", "Data changed")
-
         for (event in dataEventBuffer) {
-            if (event.type == DataEvent.TYPE_CHANGED) {
-                val path = event.dataItem.uri.path
-                if (path == "/dataPath") {
-                    val dataMap = DataMapItem.fromDataItem(event.dataItem).dataMap
-                    receivedMessage.value = dataMap.getString("message_key") ?: "No message"
+            if (event.type == DataEvent.TYPE_CHANGED && event.dataItem.uri.path == "/audio_record") {
+                val dataMap = DataMapItem.fromDataItem(event.dataItem).dataMap
+                val audioData = dataMap.getByteArray("audio")
+                val timestamp = dataMap.getLong("timestamp")
 
-                    Log.d("DataClientService", "Data received: ${receivedMessage.value}")
-                }
+                useAudio(audioData, timestamp)
             }
         }
+    }
+
+    private fun useAudio(audioData: ByteArray?, timestamp: Long) {
+        Log.d(
+            "DataClientService",
+            "Received audio data: ${audioData?.size} bytes, timestamp: $timestamp"
+        )
     }
 }
